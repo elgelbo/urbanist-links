2f24100941550db55187354b6cdb912db562c606
el_gelbo <mgelbman@gmail.com>
2019-06-18 22:29:03 -0700

    HtweetML

--- src/scripts/socket.js
+++ src/scripts/socket.js
@@ -1,8 +1,13 @@
 const socket = () => {
-  console.log('duude');
   var socket = io();
   socket.on('tweets', function (tweet) {
     console.log(tweet);
+    var parent = document.querySelector('#tweet-container');
+    console.log(parent);
+    const s = `<div id="myDiv">${tweet.text} </div>`
+    var htmlObject = document.createElement('div');
+    htmlObject.innerHTML = s;
+    parent.prepend(htmlObject);
   });
 }
 
